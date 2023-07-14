cc8bcf806c87bd87fccb27e9b6920a73c5071e52
ZachMommaerts <zmommaerts@gmail.com>
2023-07-14 12:22:43 -0500

    Initial Commit

---
+++ M2L5_Challenge/M2L5_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M2L5_ChallengeApp.swift
+//  M2L5_Challenge
+//
+//  Created by Zach Mommaerts on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct M2L5_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
